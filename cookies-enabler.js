b6808e97e9b85c5a642c90e67093e7a70b192190
Nicholas Ruggeri <nicholasruggeri@MacBook-Pro-di-Nicholas.local>
2015-05-22 17:35:29 +0200

    create module

--- cookies-enabler.js
+++ cookies-enabler.js
@@ -1,31 +1,43 @@
-var elem = document.getElementsByClassName('ce-elm'),
-    trigger = document.getElementsByClassName('ce-trigger'),
-    bar = document.getElementsByClassName('ce-bar');
+window.COOKIES_ENABLER = window.COOKIES_ENABLER || (function () {
 
-var getScripts = function(){
-    var n = elem.length;
-    for (var i = 0; i < n; i++){
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        for (var y = 0; y < elem[i].attributes.length; y++) {
-            var attrib = elem[i].attributes[y];
-            if (attrib.specified) {
-                if ((attrib.name != 'type') && (attrib.name != 'class')){
-                    s.setAttribute(attrib.name, attrib.value);
-                }
+    var elem = document.getElementsByClassName('ce-elm'),
+        trigger = document.getElementsByClassName('ce-trigger'),
+        bar = document.getElementsByClassName('ce-bar');
+
+    var init = function () {
+        if (window.sessionStorage){
+            if (sessionStorage.consent == 'Y'){
+                getScripts();
             }
         }
-        s.innerHTML = elem[i].innerHTML;
-        document.body.appendChild(s);
+
+        trigger[0].addEventListener("click", function(){
+            sessionStorage.setItem("consent", "Y");
+            getScripts();
+        });
+    };
+
+    var getScripts = function(){
+        var n = elem.length;
+        for (var i = 0; i < n; i++){
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            for (var y = 0; y < elem[i].attributes.length; y++) {
+                var attrib = elem[i].attributes[y];
+                if (attrib.specified) {
+                    if ((attrib.name != 'type') && (attrib.name != 'class')){
+                        s.setAttribute(attrib.name, attrib.value);
+                    }
+                }
+            }
+            s.innerHTML = elem[i].innerHTML;
+            document.body.appendChild(s);
+        }
+        bar[0].style.display = 'none';
     }
-    bar[0].style.display = 'none';
-}
-if (window.sessionStorage){
-    if (sessionStorage.consent == 'Y'){
-        getScripts();
-    }
-}
-trigger[0].addEventListener("click", function(){
-    sessionStorage.setItem("consent", "Y");
-    getScripts();
-});
+
+    return {
+        init: init
+    };
+
+}());
